9a3fd00be6c35a3c37456b82639445ae6f4993dc
yahirrro <yahiro0921@gmail.com>
2019-12-27 16:22:15 +0900

    👶Design Update & added categories

--- components/cards/faq.vue
+++ components/cards/faq.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="card">
-    <h1>{{ val.title }}</h1>
-    <p>{{ val.body }}</p>
+    <h1 class="ja">
+      {{ val.title_ja }}
+    </h1>
+    <h1 class="en">
+      {{ val.title_en }}
+    </h1>
+    <p class="ja">
+      {{ val.body_ja }}
+    </p>
+    <p class="en">
+      {{ val.body_en }}
+    </p>
+    <div class="badge">{{ val.category }}</div>
   </div>
 </template>
 <script>
@@ -9,3 +20,84 @@ export default {
   props: ["val"]
 };
 </script>
+<style lang="scss">
+.card {
+  position: relative;
+  border-radius: 15px;
+  padding: 2.2rem 1.5rem;
+  background: white;
+  border: 1px solid rgba(0, 0, 0, 0.07);
+  line-height: 1.7;
+  font-feature-settings: "palt" 1;
+  letter-spacing: 0.15em;
+  h1 {
+    position: relative;
+    font-weight: normal;
+    font-size: 1.2rem;
+    padding-left: 1.5rem;
+    padding-bottom: 0.7rem;
+    &.ja {
+      font-weight: bold;
+      &::after {
+        position: absolute;
+        content: "Q";
+        color: #007907;
+        top: 0;
+        left: 0;
+      }
+    }
+    &.en {
+      margin-bottom: 1.5rem;
+      border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+      padding-bottom: 1.5rem;
+      position: relative;
+      &:after {
+        position: absolute;
+        content: " ";
+        display: block;
+        border-bottom: solid 1px white;
+        bottom: -1px;
+        left: 0px;
+        width: 1.5rem;
+      }
+    }
+  }
+  p {
+    position: relative;
+    padding-left: 1.5rem;
+    padding-bottom: 0.7rem;
+    &.ja {
+      &:after {
+        position: absolute;
+        content: "A";
+        color: #007907;
+        top: 0;
+        left: 0;
+        opacity: 0.5;
+      }
+    }
+    &.en {
+    }
+  }
+  .en {
+    letter-spacing: 0.01em;
+    opacity: 0.3;
+    font-size: 0.7rem;
+    line-height: 1.5;
+  }
+
+  .badge {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    display: inline-block;
+    padding: 3px 10px;
+    border-radius: 20px;
+    border: solid 1px #007907;
+    color: #007907;
+    font-size: 0.7rem;
+    font-weight: bold;
+    opacity: 0.5;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -20,11 +20,7 @@
       </div>
     </header>
     <div class="container">
-      <card
-        v-for="items of content"
-        v-bind:key="items.title"
-        :val="items"
-      ></card>
+      <card v-for="items of content" v-bind:key="items.id" :val="items"></card>
     </div>
   </div>
 </template>
@@ -37,7 +33,7 @@ export default {
   components: { card },
   async asyncData({ app, params }) {
     const { data } = await axios.get(
-      "https://asia-northeast1-sheetstowebapi.cloudfunctions.net/api?id=1W5zD7PO3myloJJpuy-0Ir70TnMK8afuh2nQH-7EIiS0&range=content!A2:C100"
+      "https://asia-northeast1-sheetstowebapi.cloudfunctions.net/api?id=1W5zD7PO3myloJJpuy-0Ir70TnMK8afuh2nQH-7EIiS0&range=content!A2:G100"
     );
     return { content: data };
   }
@@ -87,43 +83,6 @@ header#header {
     padding-left: 20px;
     p {
       padding-top: 10px;
-    }
-  }
-}
-.card {
-  border-radius: 15px;
-  padding: 2rem 1.5rem;
-  background: white;
-  border: 1px solid rgba(0, 0, 0, 0.07);
-  line-height: 1.7;
-  font-feature-settings: "palt" 1;
-  letter-spacing: 0.11em;
-  h1 {
-    position: relative;
-    font-weight: normal;
-    font-size: 1.2rem;
-    padding-bottom: 1rem;
-    margin-bottom: 1rem;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.07);
-    padding-left: 1.5rem;
-    &::after {
-      position: absolute;
-      content: "Q";
-      color: #007907;
-      top: 0;
-      left: 0;
-    }
-  }
-  p {
-    position: relative;
-    padding-left: 1.5rem;
-    &:after {
-      position: absolute;
-      content: "A";
-      color: #007907;
-      top: 0;
-      left: 0;
-      opacity: 0.5;
     }
   }
 }
